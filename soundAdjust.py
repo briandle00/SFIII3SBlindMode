cbb08ebef1f0e4c65e9eeeac4d467052e53f4ae8
kkevindaii <50091670+kkevindaii@users.noreply.github.com>
2020-11-07 00:48:27 -0800

    Added file to handle bpm/pitch

---
+++ soundAdjust.py
@@ -0,0 +1,21 @@
+def bpm_adjustment(bpm_sound, lateral_distance):
+  if (lateral_distance == 0):
+    bpm_sound.stretch_factor = 1.0
+  else:
+    # Arbitary Numbers
+    bpm_sound.stretch_factor = (lateral_distance * 0.002)
+  return
+
+
+def pitch_adjustment(pitch_sound, vert_diff):
+  if (vert_diff == 0):
+    pitch_sound.pitch_shift = 0
+  else:
+    pitch_sound.pitch_shift = 1
+  return
+
+def removeAndReset(sound, sampler):
+    sound.stretch_factor = 1.0
+    sound.pitch_shift = 0
+    sampler.remove(sound)
+    return
